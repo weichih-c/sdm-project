5fccc80bad649ac209a5b380c8ae61de415b34e7
weichih-c <weichih1002@gmail.com>
2016-12-05 23:40:31 +0800

    Feature: 新增資料表;  更換左方明細欄目的框架，修改最上方navbar的樣式.
    Technical Details:
    1. 新增資料表(帳目 類別 子類別 付款方式 收支).
    2. 因為左方明細欄目原本套的版型無法點擊每一筆欄位資料，為了擴充性因此更換版型.
    3. 上方navbar換成跟登入頁相近的樣式.

--- src/account/models.py
+++ src/account/models.py
@@ -1,5 +1,46 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from member.models import Member
 
-# Create your models here.
+
+class Receipt(models.Model):
+    money = models.IntegerField()
+    remark = models.CharField(max_length=100)
+    date = models.DateField()
+    subclassification = models.ForeignKey('SubClassification', on_delete=models.CASCADE)
+    payment = models.ForeignKey('Payment', on_delete=models.CASCADE)
+    incomeandexpense = models.ForeignKey('IncomeAndExpense', on_delete=models.CASCADE)
+
+class Classification(models.Model):
+    CLASSIFICATION_TYPE = (
+        ('FO', 'food'),
+        ('CL', 'clothing'),
+        ('HO', 'housing'),
+        ('TR', 'transportation'),
+        ('ED', 'education'),
+        ('EN', 'entertainment'),
+        ('OT', 'others'),
+    )
+    classificaion_type = models.CharField(max_length=2, choices=CLASSIFICATION_TYPE)
+
+class SubClassification(models.Model):
+    name = models.CharField(max_length=100)
+    classification = models.ForeignKey('Classification', on_delete=models.CASCADE)
+    member = models.ForeignKey('member.Member', on_delete=models.CASCADE)
+
+class Payment(models.Model):
+    PAYMENT_TYPE = (
+        ('CR', 'credit_card'),
+        ('CA', 'cash'),
+    )
+    payment_type = models.CharField(max_length=2, choices=PAYMENT_TYPE)
+
+class IncomeAndExpense(models.Model):
+    INCOME_TYPE = (
+        ('IN', 'income'),
+        ('EX', 'expense'),
+    )
+    income_type = models.CharField(max_length=2, choices=INCOME_TYPE)
+
+    
